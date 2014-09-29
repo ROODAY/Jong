b8ac3bb89a2e2cacc6150611bfed961383914814
ROODAY <rudytheninja@gmail.com>
2014-09-28 22:12:11 -0400

    favicons

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + 'index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 var connectedUsers = [];
